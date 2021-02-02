2135c89dd2544a868667a2a245235162cac81537
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-02 19:24:20 +0200

    add

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,6 @@
 // const store = createStore(rootReducer, composeWithDevTools);
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers/mainReducers";
-console.log(process.env.NODE_ENV);
 const store = configureStore({
   reducer: rootReducer,
 });
